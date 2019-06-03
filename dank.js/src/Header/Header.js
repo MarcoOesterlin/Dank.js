dd4fac4065e91b5c8d5b06ab3855ae8e6ddaeb55
ChristopherAC <christopher.a.cervin@gmail.com>
2019-06-04 00:08:53 +0200

    header now goes black while beyoond one third of window height.

--- dank.js/src/Header/Header.js
+++ dank.js/src/Header/Header.js
@@ -1,9 +1,25 @@
 import React from 'react';
 
-class Header extends React.Component {   
+class Header extends React.Component {
+  state = {
+    headerClass: 'header transparent',
+  }
+  
+  listenScrollEvent = e => {
+    if (window.scrollY > window.innerHeight/3 ) {
+      this.setState({headerClass: 'header colored'})
+    } else {
+      this.setState({headerClass: 'header transparent'})
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.listenScrollEvent)
+  }
+
   render() {
     return (
-      <header className="header">
+      <header className={this.state.headerClass}>
         <h1>DankJS</h1>
         <nav>
           <a href="#paralax-1">Search</a>
